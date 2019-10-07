620184ad73c935c9a83300367f0bfa73ca5d7e9d
Igor Castro Santilli <igor.santilli@unisys.com>
2019-10-07 17:33:17 -0300

    Responsivo

--- js/salas.js
+++ js/salas.js
@@ -16,4 +16,30 @@ $(function(){
         $("#sala2").hide();
         $("#sala1").hide();
     });
-})
+});
+
+$(function(){
+    $("#Reserva1").click(function(){
+        $("#state1").text("Reservada");
+    });
+
+    $("#Cancelar1").click(function(){
+        $("#state1").text("Disponível");
+    });
+
+    $("#Reserva2").click(function(){
+        $("#state2").text("Reservada");
+    });
+
+    $("#Cancelar2").click(function(){
+        $("#state2").text("Disponível");
+    });
+
+    $("#Reserva3").click(function(){
+        $("#state3").text("Reservada");
+    });
+
+    $("#Cancelar3").click(function(){
+        $("#state3").text("Disponível");
+    });
+});
